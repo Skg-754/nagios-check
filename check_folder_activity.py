8e025687cc0995be4df016646826d54b225505ff
Supervision <supervision@localhost.com>
2018-05-24 09:34:22 +0000

    added wmi error support

--- check_folder_activity.py
+++ check_folder_activity.py
@@ -74,15 +74,69 @@ statusCode=None
 statusInformation=None
 perfData=None
 
+
+
+def wmiErrorAnalysis (stderr) :
+	'''
+		Analysis of the stderr of a wmi command
+		Extract the error code and give the error descr
+		:param sdterr:	the binary stderr message
+		:return: a tuple containing the error code and the error descr
+	'''
+
+	# wmi errors
+	wmiErrors = {
+		'0x8007000e' : 'Not enough storage is available to complete this operation',
+		'0x80010108' : 'The object invoked has disconnected from its clients.',
+		'0x800705af' : 'The paging file is too small for this operation to complete.',
+		'0x80010111' : 'Unknown error',
+		'0xc002001b' : 'WMI Timeout error',
+		'0x80041013' : 'Provider Load Failure'
+	}	
+
+	errorCode = stderr.decode('utf-8')[18:28]
+	errorMsg = 'Unkown error'
+	if errorCode in wmiErrors.keys() :
+		errorMsg = wmiErrors[errorCode]
+	
+	return errorCode, errorMsg
+
+
+def wmiCommandExec (request) :
+	'''
+		Try to execute a wmi command
+		If subprocess return an error code, exit the script and
+		Else, return the sdtout of the command
+	'''
+	if verbose :
+		print(request)
+	process = subprocess.Popen(request, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+
+	if process.returncode != 0 :
+		if verbose :
+			print('wmic command return and error return code')
+		errorCode, errorMsg = wmiErrorAnalysis(stderr)
+		statusInformation = 'WMI Error : {} - {}'.format(errorCode, errorMsg)
+		perfData = "'isActive'=0"
+		statusCode = nagiosStatusCode['UNKNOWN']
+
+		# nagios output - end of script
+		print('{} | {}'.format(statusInformation, perfData))
+		sys.exit(statusCode)
+	else : 
+		return stdout
+		
+	
+
 # if a shortcut file is used
 if shortcut :
 	request = 'wmic -A {} //{} \'Select Target FROM Win32_ShortcutFile WHERE Drive="{}" and Path="{}" and FileName="{}"\''.format(authFile, host, drive, path, shortcut)
-	if verbose : 
-		print(request)
-	result = subprocess.Popen(request, shell=True, stdout=subprocess.PIPE).stdout.read()
-	resultArray = result.decode('utf-8').split('|')
-	if len(resultArray) == 3 :
-		
+	
+	stdout = wmiCommandExec(request) 
+	resultArray = stdout.decode('utf-8').split('|')
+ 
+	if len(resultArray) == 3 :	
 		fullPath = resultArray[-1]
 		if verbose :
 			print(fullPath)
@@ -91,49 +145,38 @@ if shortcut :
 		userFriendlyPath = path.replace('\\\\','/')
 	else : 
 		firstResult = resultArray[0]
-		if firstResult.find('ERROR') :
-			statusInformation = 'wmi error : {}'.format(firstResult)
-		else :
-			statusInformation = 'Error : Shortcut file not found'
+		statusInformation = 'Error : Shortcut file not found'
 		perfData = "'isActive'=0"
 		statusCode = nagiosStatusCode['UNKNOWN']
-		
-		# nagios output
+	
+		# nagios output - end of script
 		print('{} | {}'.format(statusInformation, perfData))
 		sys.exit(statusCode)
 	
 	
 
-# wmi command
+# wmi command 
 request = 'wmic -A {} //{} \'Select LastModified FROM CIM_DataFile WHERE Drive="{}" AND Path="{}" and LastModified>"{}.000000+000"\''.format(authFile, host, drive, path, formattedSearchDate)
 
 if verbose :
 	print('drive : {} - authFile : {} - host : {} - path : {} - delta : {}'.format(drive, authFile, host, path, delta))
 	print(request)
 
-# execution of the wmi command for the first time
-result1 = subprocess.Popen(request, shell=True, stdout=subprocess.PIPE).stdout.read()
-
+# request 1
+result1 = wmiCommandExec(request)
 if verbose : 
 	print(result1.decode('utf-8'))
 
-# execution of the wmi command for the second time
-result2 = subprocess.Popen(request, shell=True, stdout=subprocess.PIPE).stdout.read()
-
+# request 2
+result2 = wmiCommandExec(request)
 if verbose :
 	print(result2.decode('utf-8'))
 
 # comparison of the two results
-if result1 == result2 :
-	result1 = result1.decode('utf-8')	
-	if result1.find('ERROR: Retrieve result data.') : 
-		statusInformation = 'WMI error, unable to request the folder : {}'.format(result1)
-		perfData = "'isActive'=0"
-		statusCode = nagiosStatusCode['UNKNOWN']
-	else :
-		statusInformation = 'No changes detected in the folder : {}'.format(userFriendlyPath)
-		perfData = "'isActive'=-1"
-		statusCode = nagiosStatusCode['CRITICAL']
+if result1 == result2 :	
+	statusInformation = 'No changes detected in the folder : {}'.format(userFriendlyPath)
+	perfData = "'isActive'=-1"
+	statusCode = nagiosStatusCode['CRITICAL']
 else :
 	statusInformation = 'Changes detected in the folder {}'.format(userFriendlyPath)
 	perfData = "'isActive'=1"
